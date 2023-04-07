4a7b438838c06a382251f42b557cf5ee7f514658
JustNaaby <evan.arroyo@outlook.fr>
2023-04-07 23:57:51 +0200

    Encore fix bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  url: 'https://justnaaby.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/vlife-rp-website/`,
+  baseUrl: '/vlife-rp-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
