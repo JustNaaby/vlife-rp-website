a794276822537b23a1f964be2c71f663478a6e36
JustNaaby <evan.arroyo@outlook.fr>
2023-04-08 10:35:53 +0200

    Retrait du edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,18 +39,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
