5369cf48db2d5187aeaf2afc43798fc540cf79bc
JustNaaby <evan.arroyo@outlook.fr>
2023-07-02 13:02:56 +0200

    modif baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'http://serious.vlife-roleplay.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vlife-rp-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
